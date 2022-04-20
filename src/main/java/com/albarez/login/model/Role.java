392ec81c17fe92119d72c3901e0529ae5512b6d4
edwinjs <edwinjs31@gmail.com>
2022-04-20 20:45:56 +0200

    Creando la clase Role

---
+++ src/main/java/com/albarez/login/model/Role.java
@@ -0,0 +1,22 @@
+package com.albarez.login.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private UserRole name;
+}
